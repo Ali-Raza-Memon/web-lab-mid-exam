b380ddf8280574102f532f9d0b0e7c9a5cc98610
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2024-04-02 14:48:04 +0500

    working on thumbnails

---
+++ script.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const images = ["one.svg","two.svg","three.svg"]; 
+    let currentIndex = 0; 
+  
+    const imageSlider = document.getElementById("imageSlider");
+    const prevButton = document.getElementById("prev");
+    const nextButton = document.getElementById("next");
+  
+    
+    function updateImage() {
+      imageSlider.src = images[currentIndex];
+    }
+  
+    
+    prevButton.addEventListener("click", function() {
+      currentIndex--;
+      if (currentIndex < 0) {
+        currentIndex = images.length - 1; 
+      }
+      updateImage(); 
+    });
+  
+    
+    nextButton.addEventListener("click", function() {
+      currentIndex++;
+      if (currentIndex >= images.length) {
+        currentIndex = 0; 
+      }
+      updateImage(); 
+    });
+  });
+  
+
+
+
